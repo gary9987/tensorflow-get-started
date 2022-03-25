b6b842255abc0a4f6a69779a39e6c253a21ec030
Gary <gary369880206@gmail.com>
2022-03-25 21:43:39 +0800

    update shell_script

--- incremental/run.sh
+++ incremental/run.sh
@@ -1,1 +1,4 @@
+# cifar10 (None, 32, 32, 3)
+# cifar100 (None, 32, 32, 3)
+# mnist (None, 28, 28, 1)
 python3 incremental_training.py --start 0 --end 249 --dataset_name 'cifar10' --inputs_shape '(None, 32, 32, 3)'
