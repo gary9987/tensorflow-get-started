3793a0bfa8625339144b88666a167b3468080e0b
Gary <gary369880206@gmail.com>
2022-03-25 21:38:25 +0800

    add augmentation 'mnist, cifar100'

--- incremental/augmentation.py
+++ incremental/augmentation.py
@@ -9,6 +9,10 @@ class Augmentation:
     def get_augmentation(self, name, training=True):
         if name == 'cifar10':
             return self.cifar10(training)
+        if name == 'cifar100':
+            return self.cifar100(training)
+        if name == 'mnist':
+            return self.mnist(training)
 
     def cifar10(self, training):
         rgb_mean = [0.4914, 0.4822, 0.4465]
@@ -27,4 +31,42 @@ class Augmentation:
                 tf.keras.layers.Rescaling(1. / 255),
                 tf.keras.layers.Lambda(lambda x: x - tf.constant(rgb_mean, shape=[1, 1, 3])),
                 tf.keras.layers.Lambda(lambda x: x / tf.constant(rgb_std, shape=[1, 1, 3])),
+            ])
+
+    def cifar100(self, training):
+        rgb_mean = [0.5071, 0.4867, 0.4408]
+        rgb_std = [0.2675, 0.2565, 0.2761]
+        if training:
+            return tf.keras.Sequential([
+                tf.keras.layers.Rescaling(1. / 255),
+                tf.keras.layers.Lambda(lambda x: x - tf.constant(rgb_mean, shape=[1, 1, 3])),
+                tf.keras.layers.Lambda(lambda x: x / tf.constant(rgb_std, shape=[1, 1, 3])),
+                tf.keras.layers.ZeroPadding2D(padding=4),
+                tf.keras.layers.RandomCrop(height=32, width=32, seed=self.seed),
+                tf.keras.layers.RandomFlip(mode='horizontal', seed=self.seed),
+            ])
+        else:
+            return tf.keras.Sequential([
+                tf.keras.layers.Rescaling(1. / 255),
+                tf.keras.layers.Lambda(lambda x: x - tf.constant(rgb_mean, shape=[1, 1, 3])),
+                tf.keras.layers.Lambda(lambda x: x / tf.constant(rgb_std, shape=[1, 1, 3])),
+            ])
+
+    def mnist(self, training):
+        rgb_mean = [0.1307]
+        rgb_std = [0.3081]
+        if training:
+            return tf.keras.Sequential([
+                tf.keras.layers.Rescaling(1. / 255),
+                tf.keras.layers.Lambda(lambda x: x - tf.constant(rgb_mean, shape=[1, 1, 1])),
+                tf.keras.layers.Lambda(lambda x: x / tf.constant(rgb_std, shape=[1, 1, 1])),
+                tf.keras.layers.ZeroPadding2D(padding=1),
+                tf.keras.layers.RandomCrop(height=30, width=30, seed=self.seed),
+                tf.keras.layers.RandomFlip(mode='horizontal', seed=self.seed),
+            ])
+        else:
+            return tf.keras.Sequential([
+                tf.keras.layers.Rescaling(1. / 255),
+                tf.keras.layers.Lambda(lambda x: x - tf.constant(rgb_mean, shape=[1, 1, 1])),
+                tf.keras.layers.Lambda(lambda x: x / tf.constant(rgb_std, shape=[1, 1, 1])),
             ])
