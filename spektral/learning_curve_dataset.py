7a78219c2b50da93f46608ed4f63f57f02cd73d5
Gary <gary369880206@gmail.com>
2022-05-19 14:33:03 +0800

    dataset node feature and labels OK

--- spektral/learning_curve_dataset.py
+++ spektral/learning_curve_dataset.py
@@ -1,21 +1,53 @@
 from spektral.data import Dataset, DisjointLoader, Graph
 import pickle
 import numpy as np
-
+import csv
 
 class LearningCurveDataset(Dataset):
-    def __init__(self, record_dic, **kwargs):
+    def __init__(self, record_dic, record_dir, **kwargs):
         self.nodes = 67
-        self.feats = 5
+        self.n_features = 7
+        # 'INPUT': 0, 'conv1x1-bn-relu': 1, 'conv3x3-bn-relu': 2, 'maxpool3x3': 3, 'OUTPUT': 4, 'Classifier': 5, 'maxpool2x2': 6,
+        self.ops_dict = {'INPUT': 0, 'conv1x1-bn-relu': 1, 'conv3x3-bn-relu': 2, 'maxpool3x3': 3, 'OUTPUT': 4}
+        self.record_dir = record_dir
         self.record_dic = record_dic
         super().__init__(**kwargs)
 
     def read(self):
         graph_list = []
-        for record in self.record_dic:
-            matrix, ops, layers = np.array(record['matrix']), record['ops'], record['layers']
-            adj_matrix = np.zeros((self.nodes, self.nodes))
 
+        for record in self.record_dic:
+            matrix, ops, layers, log_file = np.array(record['matrix']), record['ops'], record['layers'], record['log_file']
+
+            # Labels Y
+            y = np.zeros(3)  # train_acc, valid_acc, test_acc
+            with open(self.record_dir + log_file) as f:
+                rows = csv.DictReader(f)
+                tmp = []
+                for row in rows:
+                    tmp.append(row)
+                y[0] = tmp[-1]['accuracy']
+                y[1] = tmp[-1]['val_accuracy']
+                y[2] = tmp[-1]['test_acc']
+
+            # Node features X
+            x = np.zeros((self.nodes, self.n_features), dtype=int)
+            ops2idx = [self.ops_dict[op] for op in ops]
+            for now_layer in range(11+1):
+                if now_layer == 0:
+                    x[now_layer][2] = 1  # stem is a 'conv3x3-bn-relu' type
+                elif now_layer == 4:
+                    x[22][6] = 1  # maxpool2x2
+                elif now_layer == 8:
+                    x[44][6] = 1  # maxpool2x2
+                else:
+                    now_group = now_layer // 4 + 1
+                    node_start_no = now_group + 7 * (now_layer - now_group)
+                    for i in range(len(ops2idx)):
+                        x[i + node_start_no][ops2idx[i]] = 1
+            x[66][5] = 1
+            # Adjacency matrix A
+            adj_matrix = np.zeros((self.nodes, self.nodes))
             '''
             0 convbn 128 
             1 cell    
@@ -50,9 +82,8 @@ class LearningCurveDataset(Dataset):
                                 if matrix[i][j] == 1:
                                     adj_matrix[i + node_start_no][j + node_start_no] = 1
 
-            print(matrix)
-            print(adj_matrix)
-            pass
+
+
 
 
 if __name__ == '__main__':
@@ -60,6 +91,6 @@ if __name__ == '__main__':
     record = pickle.load(file)
     file.close()
 
-    dataset = LearningCurveDataset(record_dic=record)
+    dataset = LearningCurveDataset(record_dic=record, record_dir='../incremental/cifar10_log/')
     dataset.read()
 
