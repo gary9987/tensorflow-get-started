b7c998d318683acd1143c96968118831d44d9490
Gary Chen <guanyinc@amazon.com>
2022-06-30 19:19:36 +0800

    update query_nas_bench_101.py for new nasbench.api

--- gcn/query_nas_bench_101.py
+++ gcn/query_nas_bench_101.py
@@ -7,16 +7,17 @@ if __name__ == '__main__':
     cell_list = pickle.load(file)
     file.close()
 
-    nasbench = api.NASBench('./nas-bench-101-data/nasbench_full.tfrecord')
+    nasbench = api.NASBench('./nas-bench-101-data/nasbench_only108.tfrecord')
 
     new_list = []
     
     print('cell_list length = {}'.format(str(len(cell_list))))
     
     count = 0
-    
+    skip_count = 0
+
     for cell in cell_list:
-        
+    #for cell in cell_list[:10]:    
         if count % 10000 == 0:
             print('now processing {}'.format(str(count)))
        
@@ -24,14 +25,17 @@ if __name__ == '__main__':
         matrix = cell[0]
         try:
             ops = list(map(lambda x: x.lower(), cell[1]))
-            data = nasbench.query(api.ModelSpec(matrix=matrix, ops=ops))
+            data = [nasbench.query(api.ModelSpec(matrix=matrix, ops=ops), query_idx = i) for i in range(3)]
+            #print(data)
             new_list.append([matrix, cell[1], data])
         except:
-            print('skip with error ', matrix, cell[1])
+            skip_count += 1
+            #print('skip with error ', matrix, cell[1])
 
     with open('./nas-bench-101-data/nasbench_101_cell_list.pkl', 'wb') as f:
         pickle.dump(new_list, f)
     
+    print(f'Finish with skip {skip_count}')
         
 
 
