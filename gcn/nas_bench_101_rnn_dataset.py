0c4cb6e8b2170ff89079315ebc5afe1ebd12e098
Gary Chen <gary369880206@gmail.com>
2022-08-07 00:34:20 +0800

    To finish nas_bench_101_rnn

---
+++ gcn/nas_bench_101_rnn_dataset.py
@@ -0,0 +1,81 @@
+import logging
+import numpy as np
+import os
+import tensorflow as tf
+from tensorflow.keras import layers
+
+logging.basicConfig(filename='nas_bench_101_dataset.log', level=logging.INFO)
+
+def bfs(matrix):
+    todo = [1]
+    visit = [False] * 8
+    visit[1] = True
+    order = []
+
+    num_nodes = 7
+    while len(todo) != 0:
+        r = len(todo)
+        for i in range(r):
+            front = todo.pop(0)
+            order.append(front)
+            for j in range(1, num_nodes + 1):
+                if matrix[front][j] != 0 and visit[j] is not True:
+                    todo.append(j)
+                    visit[j] = True
+
+    return order
+
+
+def get_dataset(start, end, label_query):
+    file_path = 'NasBench101Dataset'
+    x_train = []
+    y_train = []
+
+    for no in range(start, end + 1):
+        data = np.load(os.path.join(file_path, f'graph_{no}.npz'))
+        x = data['x']
+        y = data['y']
+        a = data['a']
+
+        y = y[label_query][:3]
+
+        order = bfs(a)
+        out = np.zeros((a.shape[0], x.shape[1]), dtype=float)
+
+        for now_layer in range(12):
+            if now_layer == 0:
+                out[0] = x[0]
+            elif now_layer == 4:
+                out[22] = x[22]
+            elif now_layer == 8:
+                out[44] = x[44]
+            else:
+                now_group = now_layer // 4 + 1
+                node_start_no = now_group + 7 * (now_layer - now_group)
+                for i in range(7):
+                    try:
+                        real_idx = node_start_no + order[i] - 1
+                        out[i + node_start_no] = x[real_idx]
+                    except:
+                        pass
+
+        x_train.append(out)
+        y_train.append(y)
+
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    return x_train, y_train
+
+
+if __name__ == '__main__':
+    x_train, y_train = get_dataset(start=0, end=80000, label_query=0)
+    x_valid, y_valid = get_dataset(start=80001, end=160000, label_query=1)
+
+    model = tf.keras.Sequential()
+    model.add(layers.LSTM(128, input_shape=(x_train.shape[1], x_train.shape[2])))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dense(3))
+    print(model.summary())
+    model.compile('adam', 'mean_squared_error', metrics=['mse'])
+    model.fit(x_train, y_train, validation_data=(x_valid, y_valid), epochs=100, batch_size=64)
+
+
