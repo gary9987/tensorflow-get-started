9520d36b49d7a5cebd49461c12269ae5701fa4e2
Gary Chen <gary369880206@gmail.com>
2022-08-07 16:33:46 +0800

    rnn bfs  train nasbench-101

--- gcn/nas_bench_101_rnn_train.py
+++ gcn/nas_bench_101_rnn_train.py
@@ -6,6 +6,7 @@ from tensorflow.keras import layers
 
 logging.basicConfig(filename='nas_bench_101_dataset.log', level=logging.INFO)
 
+
 def bfs(matrix):
     todo = [1]
     visit = [False] * 8
@@ -42,22 +43,25 @@ def get_dataset(start, end, label_query):
         order = bfs(a)
         out = np.zeros((a.shape[0], x.shape[1]), dtype=float)
 
+        now_idx = 0
         for now_layer in range(12):
             if now_layer == 0:
-                out[0] = x[0]
+                out[now_idx] = x[0]
+                now_idx += 1
             elif now_layer == 4:
-                out[22] = x[22]
+                out[now_idx] = x[22]
+                now_idx += 1
             elif now_layer == 8:
-                out[44] = x[44]
+                out[now_idx] = x[44]
+                now_idx += 1
             else:
                 now_group = now_layer // 4 + 1
                 node_start_no = now_group + 7 * (now_layer - now_group)
-                for i in range(7):
-                    try:
-                        real_idx = node_start_no + order[i] - 1
-                        out[i + node_start_no] = x[real_idx]
-                    except:
-                        pass
+
+                for i in range(len(order)):
+                    real_idx = node_start_no + order[i] - 1
+                    out[now_idx] = x[real_idx]
+                    now_idx += 1
 
         x_train.append(out)
         y_train.append(y)
@@ -69,6 +73,7 @@ def get_dataset(start, end, label_query):
 if __name__ == '__main__':
     x_train, y_train = get_dataset(start=0, end=80000, label_query=0)
     x_valid, y_valid = get_dataset(start=80001, end=160000, label_query=1)
+    x_test, y_test = get_dataset(80001, 80256, 0)
 
     model = tf.keras.Sequential()
     model.add(layers.LSTM(128, input_shape=(x_train.shape[1], x_train.shape[2])))
@@ -77,5 +82,9 @@ if __name__ == '__main__':
     print(model.summary())
     model.compile('adam', 'mean_squared_error', metrics=['mse'])
     model.fit(x_train, y_train, validation_data=(x_valid, y_valid), epochs=100, batch_size=64)
+
+    pred = model.predict(x_test)
+    for i, j in zip(y_test, pred):
+        print(i, j)
 
 
