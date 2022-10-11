0f73a5e0b0327a9c5a8121bfcf279cf09d23fd87
Gary Chen <gary369880206@gmail.com>
2022-10-11 22:33:03 +0800

    add metric in nas_bench_101_analysis

--- gcn/nas_bench_101_analysis.py
+++ gcn/nas_bench_101_analysis.py
@@ -3,14 +3,32 @@ import os
 
 if __name__ == '__main__':
 
-    file_path = 'Preprocessed_NasBench101Dataset'
+    #file_path = 'Preprocessed_NasBench101Dataset'
+    file_path = 'NasBench101Dataset'
 
     out = [np.zeros((1, 3))] * 3
 
-    for no in range(0, 169594):#169594):
+    more_than_80 = 0
+    under80 = 0
+
+    for no in range(0, 169594):  #169594):
         data = np.load(os.path.join(file_path, f'graph_{no}.npz'))
         y = data['y']
 
+        for idx in range(y.shape[0]):
+            if not np.isnan(y[idx][0]):
+                if y[idx][0] < 0.8:
+                    under80 += 1
+                else:
+                    more_than_80 += 1
+                break
+
+    print(more_than_80, under80)
+    # Filtered 169042 387
+    # Non-filtered 168403 1191
+
+
+    '''
         row = []
         for i in range(3):
             """
@@ -22,7 +40,9 @@ if __name__ == '__main__':
 
             row[i] = np.array(row[i]).reshape(1, 3)
             out[i] = np.append(out[i], row[i], axis=0)
+        '''
 
+    '''
     for i, j in enumerate(['train', 'valid', 'test']):
         out[i] = np.delete(out[i], 0, axis=0)
         np.savez(f'out_{j}.npz', out=out[i])
@@ -31,5 +51,5 @@ if __name__ == '__main__':
         data = np.load(f'out_{i}.npz')['out']
         print(data.shape)
     # (169594, 3)
-
+    '''
 
