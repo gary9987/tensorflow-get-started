aa965b4c563cdb0f8e69d93358ca946171dfefa0
Gary Chen <gary369880206@gmail.com>
2022-10-28 01:13:52 +0800

    update test_nasbench.py to log diff range between gt and pred

--- gcn/test_nasbench.py
+++ gcn/test_nasbench.py
@@ -5,17 +5,19 @@ from transformation import *
 import logging
 from train_nasbench import get_weighted_mse_loss_func
 
-
 if __name__ == '__main__':
-    logging.basicConfig(filename='test.log', level=logging.INFO, force=True)
 
-    batch_size = 64
+    weight_path = 'ecc_conv_filterTrue_a436_r1_m256_b64_dropout0.2_lr0.001_mlpNone'
+
+    logging.basicConfig(filename=f'{weight_path}_test.log', level=logging.INFO, force=True)
+    batch_size = 256
     weight_alpha = 1
 
-    model = keras.models.load_model('weights', custom_objects={'weighted_mse': get_weighted_mse_loss_func(80, weight_alpha)})
+    model = keras.models.load_model(weight_path,
+                                    custom_objects={'weighted_mse': get_weighted_mse_loss_func(80, weight_alpha)})
     model.compile('adam', loss=get_weighted_mse_loss_func(mid_point=80, alpha=weight_alpha))
 
-    test_dataset = NasBench101Dataset(start=120001, end=160000, preprocessed=True)  # 80000 80250
+    test_dataset = NasBench101Dataset(start=145001, end=169593, preprocessed=True)  # 80000 80250
 
     test_dataset.apply(NormalizeParAndFlop_NasBench101())
     test_dataset.apply(RemoveTrainingTime_NasBench101())
@@ -23,10 +25,8 @@ if __name__ == '__main__':
     test_dataset.apply(NormalizeLayer_NasBench101())
     test_dataset.apply(LabelScale_NasBench101())
     test_dataset.apply(NormalizeEdgeFeature_NasBench101())
-    test_dataset.apply(RemoveEdgeFeature_NasBench101())
+    # test_dataset.apply(RemoveEdgeFeature_NasBench101())
     test_dataset.apply(SelectNoneNanData_NasBench101())
-
-    print(test_dataset)
 
     test_loader = BatchLoader(test_dataset, batch_size=batch_size, shuffle=False, epochs=1)
     loss = model.evaluate(test_loader.load(), steps=test_loader.steps_per_epoch)
@@ -40,15 +40,18 @@ if __name__ == '__main__':
     for data in test_loader:
         pred = model.predict(data[0])
         for i, j in zip(data[1], pred):
-            logging.info(f'{i} {j}')
-
+            # logging.info(f'{i} {j}')
             valid_label, valid_predict = i[1], j[1]
             diff = abs(valid_predict - valid_label)
-            delta[diff // 10] += 1
+            try:
+                delta[int(diff / 10)] += 1
+            except:
+                logging.info(f'Data out of range label: {valid_label}, pred: {valid_predict}')
 
     for i, j in enumerate(delta):
         logging.info(f'Diff range {i * 10}~{i * 10 + 9}: {j}')
 
+    '''
     test_loader = BatchLoader(test_dataset, batch_size=batch_size, shuffle=False, epochs=1)
     logging.info('******************************************************************************')
     for data in test_loader:
@@ -56,3 +59,4 @@ if __name__ == '__main__':
         for i, j in zip(data[1], pred):
             if i[0] <= 80:
                 logging.info(f'{i} {j}')
+    '''
