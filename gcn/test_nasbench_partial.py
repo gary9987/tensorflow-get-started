9ba7703fd2c6e978c5a1b4a8865e3a6ffd01b61c
Gary Chen <gary369880206@gmail.com>
2022-12-08 18:23:24 +0800

    test_nasbench_partial add mAP

--- gcn/test_nasbench_partial.py
+++ gcn/test_nasbench_partial.py
@@ -8,7 +8,7 @@ import logging
 from nasbench_model import is_weight_dir
 from scipy.stats import kendalltau
 import tensorflow as tf
-from test_nasbench_metric import randon_select_data
+from test_nasbench_metric import randon_select_data, mAP
 
 
 def test_metric_partial(log_dir, weight_path, test_dataset):
@@ -26,24 +26,9 @@ def test_metric_partial(log_dir, weight_path, test_dataset):
     logging.basicConfig(filename=log_path, level=logging.INFO, force=True)
     batch_size = 64
 
-    #model = keras.models.load_model(weight_path,
-    #                                custom_objects={'weighted_mse': tf.keras.losses.MeanSquaredError()})
-    model = keras.models.load_model(weight_path)
-
-    '''
-    test_dataset = NasBench101Dataset(start=174801, end=194617, matrix_size_list=[3, 4, 5, 6, 7], preprocessed=True)
-    print(test_dataset)
-
-    test_dataset.apply(NormalizeParAndFlop_NasBench101())
-    test_dataset.apply(RemoveTrainingTime_NasBench101())
-    test_dataset.apply(Normalize_x_10to15_NasBench101())
-    test_dataset.apply(NormalizeLayer_NasBench101())
-    test_dataset.apply(LabelScale_NasBench101())
-    test_dataset.apply(NormalizeEdgeFeature_NasBench101())
-    if 'ecc_con' not in weight_path:
-        test_dataset.apply(RemoveEdgeFeature_NasBench101())
-    test_dataset.apply(SelectNoneNanData_NasBench101())
-    '''
+    model = keras.models.load_model(weight_path,
+                                    custom_objects={'weighted_mse': tf.keras.losses.MeanSquaredError()})
+    #model = keras.models.load_model(weight_path)
 
     test_loader = BatchLoader(test_dataset, batch_size=batch_size, shuffle=False, epochs=1)
     mse = model.evaluate(test_loader.load(), steps=test_loader.steps_per_epoch)
@@ -73,24 +58,29 @@ def test_metric_partial(log_dir, weight_path, test_dataset):
     test_count = 1000
     kt_list = []
     p_list = []
+    mAP_list = []
 
     for _ in range(test_count):
         pred_list, label_list = randon_select_data(pred_array, label_array, 0, num_select, 0)
         kt, p = kendalltau(pred_list, label_list)
         kt_list.append(kt)
         p_list.append(p)
+        mAP_list.append(mAP(pred_list, label_list, 0.1))
 
     kt = sum(kt_list) / len(kt_list)
     p = sum(p_list) / len(p_list)
+    avg_mAP = sum(mAP_list) / len(mAP_list)
     logging.info(f'Avg KT rank correlation: {kt}')
     logging.info(f'Avg P value: {p}')
     logging.info(f'Std KT rank correlation: {np.std(kt_list)}')
     logging.info(f'Std P value: {np.std(p_list)}')
+    logging.info(f'Avg mAP value: {avg_mAP}')
+    logging.info(f'Std mAP value: {np.std(mAP_list)}')
 
-    return {'MSE': mse, 'MAE': mae, 'KT': kt, 'P': p}
+    return {'MSE': mse, 'MAE': mae, 'KT': kt, 'P': p, 'mAP': avg_mAP}
 
 if __name__ == '__main__':
-    log_dir = 'test_partial_model'
+    log_dir = 'test_result_partial'
     model_dir = 'partial_model'
 
     test_dataset = NasBench101Dataset(start=174801, end=194617, matrix_size_list=[3, 4, 5, 6, 7], preprocessed=True)
@@ -103,8 +93,19 @@ if __name__ == '__main__':
     test_dataset.apply(RemoveEdgeFeature_NasBench101())
     test_dataset.apply(SelectNoneNanData_NasBench101())
 
-    for filename in os.listdir():
-        if os.path.isdir(filename) and is_weight_dir(filename):
-            print(f'Now test {filename}')
-            test_metric_partial(log_dir, filename, test_dataset)
+    for model_folder in os.listdir(model_dir):
+        metrics = ['MSE', 'MAE', 'KT', 'P', 'mAP']
+        results = {i: [] for i in metrics}
 
+        for i in range(10):
+            filename = f'{model_folder}_{i}'
+            print(f'Now test {filename}')
+            result = test_metric_partial(log_dir, os.path.join(model_dir, model_folder, filename), test_dataset)
+            for m in result:
+                results[m].append(result[m])
+
+        for key in results:
+            logging.info(f'{key} mean: {sum(results[key]) / len(results[key])}')
+            logging.info(f'{key} min: {min(results[key])}')
+            logging.info(f'{key} max: {max(results[key])}')
+            logging.info(f'{key} std: {np.std(results[key])}')
