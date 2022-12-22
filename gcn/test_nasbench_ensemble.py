3ea655b26fef24126698f8cbeae79da4f8570977
Gary Chen <gary369880206@gmail.com>
2022-12-22 22:17:46 +0800

    TODO: test ensemble

---
+++ gcn/test_nasbench_ensemble.py
@@ -0,0 +1,112 @@
+import os.path
+from pathlib import Path
+import keras.models
+import numpy as np
+from spektral.data import BatchLoader
+from nas_bench_101_dataset import NasBench101Dataset
+from transformation import *
+import logging
+from nasbench_model import is_weight_dir
+from scipy.stats import kendalltau
+import tensorflow as tf
+from test_nasbench_metric import randon_select_data, mAP
+from sklearn.metrics import ndcg_score, mean_squared_error, mean_absolute_error
+from xgboost import XGBRegressor
+
+
+def test_metric_partial_ensemble(log_dir, weight_path, test_dataset):
+
+    if not os.path.exists(log_dir):
+        Path(log_dir).mkdir(parents=True, exist_ok=True)
+
+    log_path = os.path.join(log_dir, f'{Path(weight_path).name}_test.log')
+
+    if os.path.exists(log_path):
+        os.remove(log_path)
+
+    logging.basicConfig(filename=log_path, level=logging.INFO, force=True)
+
+    model = XGBRegressor()
+    model.load_model(weight_path)
+
+    pred = model.predict(test_dataset['x'])
+    mse = np.sqrt(mean_squared_error(test_dataset['y'], pred))
+    print('Test MSE loss: {}'.format(mse))
+    logging.info('Test MSE loss: {}'.format(mse))
+
+    mae = np.sqrt(mean_absolute_error(test_dataset['y'], pred))
+    print('Test MAE loss: {}'.format(mae))
+    logging.info('Test MAE loss: {}'.format(mae))
+
+    label_array = np.array([])
+    pred_array = np.array([])
+
+    test_loader = BatchLoader(test_dataset, batch_size=batch_size, shuffle=False, epochs=1)
+    for data in test_loader:
+        pred = model.predict(data[0])
+        for i, j in zip(data[1], pred):
+            # logging.info(f'{i} {j}')
+            valid_label, valid_predict = i[1], j[1]
+            label_array = np.concatenate((label_array, np.array(valid_label)), axis=None)
+            pred_array = np.concatenate((pred_array, np.array(valid_predict)), axis=None)
+
+    num_select = 100
+    test_count = 1000
+    kt_list = []
+    p_list = []
+    mAP_list = []
+    ndcg_list = []
+
+    for _ in range(test_count):
+        pred_list, label_list = randon_select_data(pred_array, label_array, 0, num_select, 0)
+        kt, p = kendalltau(pred_list, label_list)
+        kt_list.append(kt)
+        p_list.append(p)
+        mAP_list.append(mAP(pred_list, label_list, 0.1))
+        ndcg_list.append(ndcg_score(np.asarray([label_list]), np.asarray([pred_list])))
+
+    kt = sum(kt_list) / len(kt_list)
+    p = sum(p_list) / len(p_list)
+    avg_mAP = sum(mAP_list) / len(mAP_list)
+    ndcg = sum(ndcg_list) / len(ndcg_list)
+    logging.info(f'Avg KT rank correlation: {kt}')
+    logging.info(f'Avg P value: {p}')
+    logging.info(f'Std KT rank correlation: {np.std(kt_list)}')
+    logging.info(f'Std P value: {np.std(p_list)}')
+    logging.info(f'Avg mAP value: {avg_mAP}')
+    logging.info(f'Std mAP value: {np.std(mAP_list)}')
+    logging.info(f'Avg ndcg value: {ndcg}')
+    logging.info(f'Std ndcg value: {np.std(ndcg_list)}')
+
+    return {'MSE': mse, 'MAE': mae, 'KT': kt, 'P': p, 'mAP': avg_mAP, 'NDCG': ndcg}
+
+if __name__ == '__main__':
+    log_dir = 'test_result_partial'
+    model_dir = 'partial_model'
+
+    test_dataset = NasBench101Dataset(start=174801, end=194617, matrix_size_list=[3, 4, 5, 6, 7], preprocessed=True)
+    test_dataset.apply(NormalizeParAndFlop_NasBench101())
+    test_dataset.apply(RemoveTrainingTime_NasBench101())
+    test_dataset.apply(Normalize_x_10to15_NasBench101())
+    test_dataset.apply(NormalizeLayer_NasBench101())
+    test_dataset.apply(LabelScale_NasBench101())
+    test_dataset.apply(NormalizeEdgeFeature_NasBench101())
+    test_dataset.apply(RemoveEdgeFeature_NasBench101())
+    test_dataset.apply(SelectNoneNanData_NasBench101())
+
+    for model_folder in os.listdir(model_dir):
+        metrics = ['MSE', 'MAE', 'KT', 'P', 'mAP', 'NDCG']
+        results = {i: [] for i in metrics}
+
+        for i in range(10):
+            filename = f'{model_folder}_{i}'
+            print(f'Now test {filename}')
+            result = test_metric_partial_ensemble(log_dir, os.path.join(model_dir, model_folder, filename), test_dataset)
+            for m in result:
+                results[m].append(result[m])
+
+        for key in results:
+            logging.info(f'{key} mean: {sum(results[key]) / len(results[key])}')
+            logging.info(f'{key} min: {min(results[key])}')
+            logging.info(f'{key} max: {max(results[key])}')
+            logging.info(f'{key} std: {np.std(results[key])}')
--- gcn/train_nasbench_ensemble.py
+++ gcn/train_nasbench_ensemble.py
@@ -10,6 +10,7 @@ import logging
 from test_nasbench_partial import test_metric_partial
 from nas_bench_101_dataset_partial import NasBench101DatasetPartial
 from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error
 
 
 def train(model_output_dir, run: int, data_size: int):
@@ -32,34 +33,28 @@ def train(model_output_dir, run: int, data_size: int):
     is_filtered = True
 
     model = XGBRegressor(**hp)
-    '''
-    model = GNN_Model(n_hidden=model_hidden, mlp_hidden=mlp_hidden, activation=model_activation, dropout=model_dropout)
 
-    # Set logger
-    if mlp_hidden is not None:
-        weight_file_dir = model.graph_conv.name + f'_filter{is_filtered}_a{weight_alpha}_size{data_size}_r{repeat}_m{model_hidden}_b{batch_size}_dropout{model_dropout}_lr{lr}_mlp{tuple(mlp_hidden)}'
-        weight_filename = model.graph_conv.name + f'_filter{is_filtered}_a{weight_alpha}_size{data_size}_r{repeat}_m{model_hidden}_b{batch_size}_dropout{model_dropout}_lr{lr}_mlp{tuple(mlp_hidden)}_{run}'
-    else:
-        weight_file_dir = model.graph_conv.name + f'_filter{is_filtered}_a{weight_alpha}_size{data_size}_r{repeat}_m{model_hidden}_b{batch_size}_dropout{model_dropout}_lr{lr}_mlp{mlp_hidden}'
-        weight_filename = model.graph_conv.name + f'_filter{is_filtered}_a{weight_alpha}_size{data_size}_r{repeat}_m{model_hidden}_b{batch_size}_dropout{model_dropout}_lr{lr}_mlp{mlp_hidden}_{run}'
+    weight_file_dir = f'xgb_size{data_size}'
+    weight_filename = f'{weight_file_dir}_{run}'
 
     Path(os.path.join(model_output_dir, weight_file_dir)).mkdir(parents=True, exist_ok=True)
     weight_full_name = os.path.join(model_output_dir, weight_file_dir, weight_filename)
 
     print(weight_full_name)
 
-    if not os.path.exists('valid_log'):
-        os.mkdir('valid_log')
+    log_dir = f'{model_output_dir}_log'
+    if not os.path.exists(os.path.join(log_dir, 'valid_log')):
+        Path(os.path.join(log_dir, 'valid_log')).mkdir(parents=True, exist_ok=True)
 
-    logging.basicConfig(filename=f'valid_log/{weight_filename}.log', level=logging.INFO, force=True, filemode='w')
+    logging.basicConfig(filename=os.path.join(log_dir, 'valid_log', f'{weight_filename}.log'), level=logging.INFO, force=True, filemode='w')
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logging.getLogger().addHandler(handler)
-    '''
+
     datasets = train_valid_test_split_dataset(NasBench101DatasetPartial(start=0, end=174800, size=data_size, matrix_size_list=[3, 4, 5, 6, 7],
-                                                                        select_seed=run, preprocessed=True), ratio=[0.9, 0.1])
+                                                                        select_seed=run, preprocessed=is_filtered), ratio=[0.9, 0.1])
     datasets['test'] = NasBench101Dataset(start=174801, end=194617, matrix_size_list=[3, 4, 5, 6, 7], preprocessed=is_filtered)
     # 194617
 
@@ -72,43 +67,26 @@ def train(model_output_dir, run: int, data_size: int):
         datasets[key].apply(Flatten4Ensemble_NasBench101())
         logging.info(f'key {datasets[key]}')
 
-
     for key, graphs in datasets.items():
         x_list = [np.squeeze(graph.a) for graph in graphs]
         y_list = [np.squeeze(graph.y) for graph in graphs]
         datasets[key] = {'x': np.array(x_list), 'y': np.array(y_list)}
 
-    model.fit(X=datasets['train']['x'], y=datasets['train']['y'], eval_set=[(datasets['valid']['x'], datasets['valid']['y'])], verbose=2)
-    '''
-    train_loader = BatchLoader(datasets['train'], batch_size=batch_size, shuffle=True)
-    valid_loader = BatchLoader(datasets['valid'], batch_size=batch_size, shuffle=False)
+    model.fit(X=datasets['train']['x'], y=datasets['train']['y'], eval_set=[(datasets['valid']['x'], datasets['valid']['y'])])
 
-    if not os.path.exists('learning_curve'):
-        os.mkdir('learning_curve')
-    history = model.fit(train_loader.load(), steps_per_epoch=train_loader.steps_per_epoch,
-                        validation_data=valid_loader.load(), validation_steps=valid_loader.steps_per_epoch,
-                        epochs=train_epochs,
-                        callbacks=[EarlyStopping(patience=patience, restore_best_weights=True),
-                                   CSVLogger(f"learning_curve/{weight_filename}_history.log")])
-
-    logging.info(f'{model.summary()}')
 
     logging.info(f'Model will save to {weight_full_name}')
-    model.save(weight_full_name)
+    model.save_model(weight_full_name)
 
-    test_loader = BatchLoader(datasets['test'], batch_size=batch_size, shuffle=False, epochs=1)
-    # model.compile('adam', loss=get_weighted_mse_loss_func(mid_point=80, alpha=1))
-    loss = model.evaluate(test_loader.load(), steps=valid_loader.steps_per_epoch)
-    logging.info('Test loss: {}'.format(loss))
+    pred = model.predict(datasets['test']['x'])
+    loss = np.sqrt(mean_squared_error(datasets['test']['y'], pred))
+    logging.info('Test MSE: {}'.format(loss))
 
-    test_loader = BatchLoader(datasets['test'], batch_size=batch_size, shuffle=False, epochs=1)
-    for data in test_loader:
-        pred = model.predict(data[0])
-        for i, j in zip(data[1], pred):
-            logging.info(f'{i} {j}')
+    for y, predict in zip(datasets['test']['y'], pred):
+        logging.info(f'{y} {predict}')
 
     return test_metric_partial('test_nasbench_partial_nochannel', weight_full_name, datasets['test'])
-    '''
+
 
 
 def train_n_runs(model_output_dir: str, n: int, data_size: int, no_channel=False):
@@ -117,7 +95,7 @@ def train_n_runs(model_output_dir: str, n: int, data_size: int, no_channel=False
 
     for i in range(n):
         # {'MSE': mse, 'MAE': mae, 'KT': kt, 'P': p}
-        metrics = train(model_output_dir, i, data_size, no_channel=no_channel)
+        metrics = train(model_output_dir, i, data_size)
         print(metrics)
         for m in metrics:
             results[m].append(metrics[m])
@@ -142,6 +120,6 @@ def parse_args():
 if __name__ == '__main__':
     args = parse_args()
     Path(args.model_output_dir).mkdir(exist_ok=True)
-    train(args.model_output_dir, 0, 100000)
+    train(args.model_output_dir, 0, 1000)
     #for i in range(500, 10501, 500):
     #    train_n_runs(args.model_output_dir, n=10, data_size=i, no_channel=True)
