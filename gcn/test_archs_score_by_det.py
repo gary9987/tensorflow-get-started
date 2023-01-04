fa9817e3f60fdbb8d509e3d6d02db6e736d15a00
Gary Chen <gary369880206@gmail.com>
2023-01-04 17:17:27 +0800

    fininsh det exp pipeline

--- gcn/test_archs_score_by_det.py
+++ gcn/test_archs_score_by_det.py
@@ -1,14 +1,19 @@
 import pickle
+import random
 import time
 from typing import Union, Tuple, List
 import tensorflow as tf
 import numpy as np
+from sklearn.metrics import ndcg_score
+from tqdm import tqdm
+
 from model_util import calculate_dataset_level
 from tensorflow.keras.datasets import cifar10
 import os
 import wget
 from model_spec import ModelSpec
 from scipy.stats import kendalltau
+from test_nasbench_metric import randon_select_data, mAP
 
 
 def setup():
@@ -39,35 +44,68 @@ def get_balanced_n_data(n: int, num_class: int, x, y):
     return np.array(x_list), np.array(y_list)
 
 
-def get_arch_score_and_acc_list(query_idx: int, sample_x: Union[tf.Tensor, np.ndarray]) -> Tuple[List, List]:
-    score_list = []
-    valid_acc_list = []
-    input_shape = sample_x.shape
-
+def get_all_arch_list(shuffle=False):
+    cell_list = []
     for matrix_size in range(3, 4):
         with open(os.path.join('nas-bench-101-data', f'nasbench_101_cell_list_{matrix_size}.pkl'), 'rb') as f:
-            cell_list = pickle.load(f)
+            cell_list += pickle.load(f)
 
-        for cell in cell_list:
-            score = calculate_dataset_level(ModelSpec(cell[0], cell[1]), input_shape, sample_x)
-            val_acc = cell[2][query_idx]['validation_accuracy']
-            print(score, val_acc)
-            score_list.append(score)
-            valid_acc_list.append(val_acc)
+    if shuffle:
+        random.shuffle(cell_list)
+
+    return cell_list
+
+
+def get_arch_score_and_acc_list(query_idx: int, sample_arch: List, sample_data: Union[tf.Tensor, np.ndarray]) -> Tuple[List, List]:
+    score_list = []
+    valid_acc_list = []
+    input_shape = sample_data.shape
+
+    for cell in tqdm(sample_arch):
+        score = calculate_dataset_level(ModelSpec(cell[0], cell[1]), input_shape, sample_data)
+        val_acc = cell[2][query_idx]['validation_accuracy']
+        #print(f'score {score} val_acc {val_acc}')
+        score_list.append(score)
+        valid_acc_list.append(val_acc)
 
     return score_list, valid_acc_list
 
 
 if __name__ == '__main__':
     setup()
-
+    cell_list = get_all_arch_list(shuffle=True)
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    x, _ = get_balanced_n_data(20, 10, x_train, y_train)
+    sample_data, _ = get_balanced_n_data(20, 10, x_train, y_train)
     query_idx = 0
 
-    score_list, val_acc_list = get_arch_score_and_acc_list(query_idx, x)
-    kt, p = kendalltau(score_list, val_acc_list)
-    print(kt, p)
+    num_select = 100
+    test_count = 1000
+    kt_list = []
+    p_list = []
+    mAP_list = []
+    ndcg_list = []
 
+    for i in range(test_count):
+        print(f'Now testing {i} run')
+        sample_arch = random.choices(cell_list, k=num_select)
+        score_list, val_acc_list = get_arch_score_and_acc_list(query_idx, sample_arch, sample_data)
+        kt, p = kendalltau(score_list, val_acc_list)
+        kt_list.append(kt)
+        p_list.append(p)
+        mAP_list.append(mAP(score_list, val_acc_list, 0.1))
+        ndcg_list.append(ndcg_score(np.asarray([score_list]), np.asarray([val_acc_list])))
+
+    kt = sum(kt_list) / len(kt_list)
+    p = sum(p_list) / len(p_list)
+    avg_mAP = sum(mAP_list) / len(mAP_list)
+    ndcg = sum(ndcg_list) / len(ndcg_list)
+    print(f'Avg KT rank correlation: {kt}')
+    print(f'Avg P value: {p}')
+    print(f'Std KT rank correlation: {np.std(kt_list)}')
+    print(f'Std P value: {np.std(p_list)}')
+    print(f'Avg mAP value: {avg_mAP}')
+    print(f'Std mAP value: {np.std(mAP_list)}')
+    print(f'Avg ndcg value: {ndcg}')
+    print(f'Std ndcg value: {np.std(ndcg_list)}')
 
 
